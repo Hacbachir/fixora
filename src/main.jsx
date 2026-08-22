f5577ad4aaa0ea98d138e526ecda9ff4e8654cc1
Bachir Hachani <hacbachir3@gmail.com>
2026-08-22 16:57:13 +0100

    Fix Google Analytics setup

--- src/main.jsx
+++ src/main.jsx
@@ -5,23 +5,6 @@ import "./index.css"
 import "./App.css"
 import App from "./App.jsx"
 
-const GA_MEASUREMENT_ID = "G-77D97DW6MP"
-
-const script = document.createElement("script")
-script.async = true
-script.src =
-  "https://www.googletagmanager.com/gtag/js?id=" + GA_MEASUREMENT_ID
-document.head.appendChild(script)
-
-window.dataLayer = window.dataLayer || []
-
-function gtag() {
-  window.dataLayer.push(arguments)
-}
-
-gtag("js", new Date())
-gtag("config", GA_MEASUREMENT_ID)
-
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
